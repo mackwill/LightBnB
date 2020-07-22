0055826c9cd30a637a8324ec98a60865716967a6
William <mack.mindenhall@gmail.com>
2020-07-22 14:56:18 +0000

    getUserWithEmail, getUserWithID and addUser functions now access the db for data

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -18,15 +18,25 @@ const pool = new Pool({
  */
 const getUserWithEmail = function (email) {
   let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+
+  return pool
+    .query(
+      `
+  SELECT * FROM users
+  WHERE email = $1;`,
+      [email]
+    )
+    .then((res) => {
+      for (const userId in res.rows) {
+        user = res.rows[userId];
+        if (user.email.toLowerCase() === email.toLowerCase()) {
+          break;
+        } else {
+          user = null;
+        }
+      }
+      return user;
+    });
 };
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -36,7 +46,12 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`SELECT * FROM users WHERE id = $1;`, [id]).then((res) => {
+    if (res.rows.length > 0) {
+      return res.rows[0];
+    }
+    return null;
+  });
 };
 exports.getUserWithId = getUserWithId;
 
@@ -46,10 +61,18 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query(
+      `
+  INSERT INTO users (name, email, password) 
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `,
+      [user.name, user.email, user.password]
+    )
+    .then((res) => {
+      return res.rows[0];
+    });
 };
 exports.addUser = addUser;
 
@@ -85,7 +108,6 @@ const getAllProperties = function (options, limit = 10) {
       [limit]
     )
     .then((res) => {
-      console.log("promise response: ", res.rows);
       for (let i = 1; i <= limit; i++) {
         limitedProperties[i] = res.rows[i];
       }
