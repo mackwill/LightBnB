bd3870cbbbffb5cc3d19826c2cb4d75c66c31e03
William <mack.mindenhall@gmail.com>
2020-07-23 10:38:31 +0000

    Modified folder structure lightly

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,15 +1,4 @@
-const properties = require("./json/properties.json");
-const users = require("./json/users.json");
-const { Pool } = require("pg");
-
-const pool = new Pool({
-  user: "vagrant",
-  password: "123",
-  host: "localhost",
-  database: "lightbnb",
-});
-
-/// Users
+const pool = require("./db/pool");
 
 /**
  * Get a single user from the database given their email.
